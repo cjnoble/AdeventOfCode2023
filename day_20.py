62111453ff3e584d14db63f17227399986ba8a86
Chris Noble <46852222+cjnoble@users.noreply.github.com>
2023-12-21 12:58:15 +0000

    Update day_20.py

--- day_20.py
+++ day_20.py
@@ -1,6 +1,6 @@
 import re
 from enum import Enum
-from collections import defaultdict
+from collections import defaultdict, deque
 
 def read_text_file (file_path):
 
@@ -14,35 +14,42 @@ class Pulse(Enum):
     Low = 0
     High = 1
 
-
 class Module(object):
     @classmethod
-    def from_string(cls, outputs_string):
+    def from_string(cls, name, outputs_string):
         #outputs = re.findall("([A-Za-z]+,*)", outputs_string)
         outputs = outputs_string.split(",")
         outputs = [i.strip() for i in outputs]
-        return cls(outputs)
+        return cls(name, outputs)
     
 class FlipFlop(Module):
     prefix = "%"
-    def __init__(self, output_modules:list):
+    def __init__(self, name, output_modules:list):
+        self.name = name
         self.output = Pulse.Low
         self.output_modules = output_modules
         self.pulse_count = {Pulse.Low: 0, Pulse.High: 0}
     
-    def update(self, input:Pulse):
-        if input is Pulse.Low:
+    def update(self, instruction):
+        input = instruction.pulse
+        if input == Pulse.Low:
             if self.output == Pulse.Low:
                 self.output = Pulse.High
             else: # Current output is high
                 self.output = Pulse.Low
         
-        self.pulse_count[self.output] += 1
+            self.pulse_count[self.output] += 1
+            return [Instruction(self.name, module, self.output) for module in self.output_modules]
+        elif input == Pulse.High:
+            pass #do nothing
+        else:
+            raise Exception(f"Unknown instruction {instruction}")
 
 class Conjunction (Module):
     prefix = "&"
-    def __init__(self, output_modules:list):
+    def __init__(self, name, output_modules:list):
         #self.inputs = {input_module: Pulse.Low for input_module in input_modules}
+        self.name = name
         self.inputs = {}
         self.output = Pulse.Low
         self.output_modules = output_modules
@@ -52,7 +59,13 @@ class Conjunction (Module):
     def add_input(self, input_str):
         self.inputs[input_str] = Pulse.Low
 
-    def update(self, input_module: str, input:Pulse):
+    def update(self, instruction):
+        '''
+        input_module: str, input:Pulse
+        '''
+        input_module = instruction.input
+        input = instruction.pulse
+
         self.inputs[input_module] = input
         if Pulse.Low in self.inputs.values():
             self.output = Pulse.High
@@ -60,29 +73,40 @@ class Conjunction (Module):
             self.output = Pulse.Low
         
         self.pulse_count[self.output] += 1
+        return [Instruction(self.name, module, self.output) for module in self.output_modules]
 
 class Broadcaster(Module):
-    def __init__(self, output_modules:list):
+    def __init__(self, name, output_modules:list):
+        self.name = name
         self.output = Pulse.Low
         self.output_modules = output_modules
         self.pulse_count = {Pulse.Low: 0, Pulse.High: 0}
 
-    def update(self, input:Pulse):
-        self.output = input
+    def update(self, instruction):
+        #input:Pulse):
+        self.output = instruction.pulse
         self.pulse_count[self.output] += 1
+        return [Instruction(self.name, module, self.output) for module in self.output_modules]
 
 class Button(Module):
-    def __init__(self, output_modules:list):
+    def __init__(self, name, output_modules:list):
+        self.name = name
         self.output = Pulse.Low
         self.output_modules = output_modules
         self.pulse_count = {Pulse.Low: 0, Pulse.High: 0}
 
-    def update(self):
+    def update(self, instruction):
         self.output = Pulse.Low
         self.pulse_count[self.output] += 1
+        return [Instruction(self.name, module, self.output) for module in self.output_modules]
 
 class Output(Module):
-    def update(self):
+    def __init__(self, name):
+        self.name = name
+        self.output_modules = []
+        self.pulse_count = {Pulse.Low: 0, Pulse.High: 0}
+
+    def update(self, instruction):
         pass
 
 def parse_inputs(data):
@@ -93,34 +117,73 @@ def parse_inputs(data):
         name, outputs = row.split("->")
         name = name.strip()
         if name[0] == "&":
-            modules[name[1:]] = Conjunction.from_string(outputs)
+            modules[name[1:]] = Conjunction.from_string(name[1:], outputs)
         elif name[0] == "%":
-            modules[name[1:]] = FlipFlop.from_string(outputs)
+            modules[name[1:]] = FlipFlop.from_string(name[1:], outputs)
         elif name == "broadcaster":
-            modules[name] = Broadcaster.from_string(outputs)
+            modules[name] = Broadcaster.from_string(name,outputs)
         elif name == "output":
-            modules[name] = Output()
+            modules[name] = Output(name)
 
     return modules
 
-def part_1(data, button_presses):
 
-    # Inital setup
-    button = Button(["broadcaster"])
+def set_up(data):
+    button = Button("button", ["broadcaster"])
     modules = parse_inputs(data)
     modules["button"] = button
 
-    print(modules)
+    #print(modules)
 
     for name, module in modules.items():
-        print(f"{name}, {module}, {module.output_modules}")
+        #print(f"{name}, {module}, {module.output_modules}")
         for output in module.output_modules:
             
             output_module = modules[output]
             if isinstance(output_module, Conjunction):
                 output_module.add_input(name)
 
-    return
+    return modules
+
+class Instruction(object):
+
+    def __init__(self, input, output, pulse):
+        self.input = input
+        self.output = output
+        self.pulse = pulse
+
+    def __repr__(self):
+        return f"{self.input}, {self.output}, {self.pulse}"
+
+
+def part_1(data, button_presses):
+
+    # Inital setup
+    modules = set_up(data)
+
+    instructions = deque()
+    instructions.append(Instruction("", "button", None))
+
+    while True:
+        #print(instructions)
+        new_instructions = deque()
+        while instructions:
+            instruction = instructions.popleft()
+            output_module = modules[instruction.output]
+
+            next_instructions = output_module.update(instruction)
+            #print(next_instructions)
+            if next_instructions:
+                new_instructions.extend(next_instructions)
+        if new_instructions:
+            instructions = new_instructions
+        else:
+            break
+
+    low_pulse_count = sum([m.pulse_count[Pulse.Low] for m in modules.values()])
+    high_pulse_count = sum([m.pulse_count[Pulse.High] for m in modules.values()])
+    print(f"Low pulses sent {low_pulse_count}, High pulses sent {high_pulse_count}")
+    return low_pulse_count * high_pulse_count
 
 def part_2(data):
 
