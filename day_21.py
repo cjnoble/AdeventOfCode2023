aaea402bca35ea96b4c3a65e2d32e893fdfe4c9d
Christopher Noble <Christopher.Noble@arup.com>
2024-10-28 10:24:22 +0000

    Day 21 WIP

--- day_21.py
+++ day_21.py
@@ -1,4 +1,6 @@
 import re
+from collections import deque
+from functools import cache
 
 def read_text_file (file_path):
 
@@ -9,17 +11,81 @@ def read_text_file (file_path):
     return data
 
 
-def part_1(data):
+class Gardens(object):
+    def __init__(self, data) -> None:
+        self.data = data
 
-    return
+    def find_start(self):
 
-def part_2(data):
+        for y, row in enumerate(self.data):
+            for x, val in enumerate(row):
+                if val == "S":
+                    return x, y
+                
+        raise Exception("Start not found")
 
-    return
+    def get_adjacent_points(self, point):
+
+        x, y = point
+
+        points = []
+
+        if x-1 >= 0:
+            points.append((x-1, y))
+        if x+1 < len(self.data[y]):
+            points.append((x+1, y))
+        if y+1 < len(self.data):
+            points.append((x, y+1))
+        if y-1 >= 0:
+            points.append((x, y-1))
+
+        return points
+
+
+    @cache
+    def get_valid_next_points(self, point):
+        next_points = []
+        for next_point in self.get_adjacent_points(point):
+            if self.data[next_point[1]][next_point[0]] != "#":
+                next_points.append(next_point)
+        return next_points
+
+
+def visit_gardens(data, step_limit):
+    data = Gardens(data)
+
+    x_start, y_start = data.find_start()
+
+    visited = set()
+    queue = set()
+    queue.add((x_start, y_start))
+
+    for step in range(step_limit):
+        new_queue = set()
+
+        for point in queue:
+            visited.add(point)
+
+            new_queue.update(data.get_valid_next_points(point))
+
+        queue = new_queue
+
+    print(queue)
+
+    return len(queue)
+
+def part_1(data, step_limit):
+
+    return visit_gardens(data, step_limit)
+
+def part_2(data, step_limit):
+
+    return visit_gardens(data, step_limit)
+
 
 if __name__ == "__main__":
 
     DAY = "21"
     data = read_text_file(f"{DAY}.txt")
-    print(part_1(data))
-    print(part_2(data))
+    print(part_1(data, 64))
+    print(part_2(data, 26501365))
--- day_21_test.py
+++ day_21_test.py
@@ -8,17 +8,82 @@ class TestMethods(unittest.TestCase):
 
     def test_1(self):
 
-        expected = 0
+        expected = 16
 
-        result = day.part_1(test_data)
+        result = day.part_1(test_data, 6)
 
         self.assertEqual(result, expected)
 
-    def test_2(self):
+    def test_p1_1(self):
 
-        expected = 0
+        steps = 0
+        expected = 1
 
-        result = day.part_2(test_data)
+        result = day.part_1(test_data, steps)
+
+        self.assertEqual(result, expected)
+
+    def test_p1_2(self):
+
+        steps = 1
+        expected = 2
+
+        result = day.part_1(test_data, steps)
+
+        self.assertEqual(result, expected)
+
+    def test_p1_3(self):
+
+        steps = 2
+        expected = 4
+
+        result = day.part_1(test_data, steps)
+
+        self.assertEqual(result, expected)
+
+    def test_p2_1(self):
+
+        steps = 6
+        expected = 16
+
+        result = day.part_2(test_data, steps)
+
+        self.assertEqual(result, expected)
+
+    def test_p2_2(self):
+
+        steps = 10
+        expected = 50
+
+        result = day.part_2(test_data, steps)
+
+        self.assertEqual(result, expected)
+
+    def test_p2_3(self):
+
+        steps = 50
+        expected = 1594
+
+        result = day.part_2(test_data, steps)
+
+        self.assertEqual(result, expected)
+
+
+    def test_p2_4(self):
+
+        steps = 100
+        expected = 6536
+
+        result = day.part_2(test_data, steps)
+
+        self.assertEqual(result, expected)
+
+    def test_p2_5(self):
+
+        steps = 500
+        expected = 167004
+
+        result = day.part_2(test_data, steps)
 
         self.assertEqual(result, expected)
 
